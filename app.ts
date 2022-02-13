8cee8beccd02932980198cd1c86fb48a5d551dc7
Efren Rodriguez <efrodriguez@redventures.com>
2022-02-13 16:30:40 -0400

    remove console log

--- app.ts
+++ app.ts
@@ -7,8 +7,6 @@ import { ApplicationStage } from "./stage/ApplicationStage";
 const app = new cdk.App();
 const region = "us-east-1";
 
-console.log(process.env);
-
 new ApplicationStage(app, "dev", {
   account: process.env.NONPROD_ACCOUNT,
   region,
